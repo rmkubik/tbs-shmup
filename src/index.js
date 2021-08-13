c23cb411af23e005a3b4765b691cda7a58b406c6
Ryan Kubik <rmkubik@me.com>
2021-08-13 11:15:28 -0700

    Asteroid movement and imagers

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import { h, render } from "preact";
-import { useState } from "preact/hooks";
+import { useState, useEffect } from "preact/hooks";
+import warningIcon from "../assets/warning.png";
+import shipIcon from "../assets/ship.png";
+import asteroidIcon from "../assets/asteroid.png";
 
 const areIndicesAdjacent = (a, b, colCount) => {
   return a - 1 === b || a + 1 === b || a - colCount === b || a + colCount === b;
@@ -14,13 +17,42 @@ const getIndicesInRange = (entity, colCount) => {
   );
 };
 
+const setTargetIndex = (colCount) => (entity) => {
+  if (!entity.targetIndex) {
+    // if no targetIndex set, set one
+    return {
+      ...entity,
+      targetIndex: entity.index + entity.speed * colCount,
+    };
+  }
+
+  return entity;
+};
+
+const clearAllTargetEntities = (entities) =>
+  entities.map(({ targetIndex, ...entity }) => entity);
+
+const isEntityDoneMoving = (entity) => entity.index === entity.targetIndex;
+
+const moveEntity = (colCount) => (entity) => {
+  if (isEntityDoneMoving(entity)) {
+    // We're at target index, just return unedited
+    return entity;
+  }
+
+  return {
+    ...entity,
+    index: entity.index + colCount,
+  };
+};
+
 const Grid = ({ tiles, colCount, renderTile }) => {
   return (
     <div
       style={{
         display: "grid",
-        gridTemplateColumns: "32px ".repeat(colCount),
-        lineHeight: "32px",
+        gridTemplateColumns: "16px ".repeat(colCount),
+        lineHeight: "16px",
         textAlign: "center",
       }}
     >
@@ -30,30 +62,72 @@ const Grid = ({ tiles, colCount, renderTile }) => {
 };
 
 const colCount = 10;
+const frameRate = 200;
 const initialTiles = new Array(150).fill({ name: "." });
-
-// initialTiles[2] = { name: "🪨" };
-// initialTiles[12] = { name: "⚠️" };
-// initialTiles[22] = { name: "⚠️" };
-// initialTiles[32] = { name: "⚠️" };
-
-// initialTiles[18] = { name: "🪨" };
-// initialTiles[28] = { name: "⚠️" };
-// initialTiles[38] = { name: "⚠️" };
-// initialTiles[48] = { name: "⚠️" };
 
 const App = () => {
   const [tiles, setTiles] = useState(initialTiles);
   const [playerIndex, setPlayerIndex] = useState(145);
   const [entities, setEntities] = useState([
-    { name: "🪨", speed: 3, index: 2 },
-    { name: "🪨", speed: 3, index: 18 },
+    { name: "🪨", speed: 3, index: 2, img: asteroidIcon },
+    { name: "🪨", speed: 3, index: 18, img: asteroidIcon },
+    { name: "🪨", speed: 6, index: -6, img: asteroidIcon },
   ]);
+  // waiting, targeting, animating
+  const [gameState, setGameState] = useState("waiting");
+
+  const moveEntities = () => {
+    const newEntities = entities.map(moveEntity(colCount));
+
+    if (newEntities.every(isEntityDoneMoving)) {
+      const resetEntities = clearAllTargetEntities(newEntities);
+
+      setEntities(resetEntities);
+      setGameState("waiting");
+    } else {
+      setEntities(newEntities);
+
+      return setTimeout(moveEntities, frameRate);
+    }
+  };
+
+  useEffect(() => {
+    let timeout;
+
+    if (gameState === "targeting") {
+      const newEntities = entities.map(setTargetIndex(colCount));
+
+      setGameState("animating");
+      setEntities(newEntities);
+
+      // animate entities turns
+      // - mark target destination
+      // - move one step
+      // - set timeout
+      // - animate next step
+      // - once reached target, setState waiting, clear targetDest
+    }
+
+    if (gameState === "animating") {
+      timeout = moveEntities();
+
+      // console.log(newEntities);
+    }
+
+    return () => clearTimeout(timeout);
+  }, [entities, gameState]);
 
   const movePlayer = (newIndex) => {
+    if (gameState !== "waiting") {
+      // Skip player input unless we're waiting
+      return;
+    }
+
     // Can only move to adjacent tile
     if (areIndicesAdjacent(newIndex, playerIndex, colCount)) {
       setPlayerIndex(newIndex);
+
+      setGameState("targeting");
     }
   };
 
@@ -65,7 +139,10 @@ const App = () => {
         getIndicesInRange(entity, colCount).includes(index)
       )
     ) {
-      object = { name: "⚠️" };
+      // TODO: when we're animating, don't show the full range
+      // of the new warning. Only show what isn't yet animated.
+      // After animation resolution, show new threat range.
+      object = { name: "⚠️", img: warningIcon };
     }
 
     const entity = entities.find((entity) => entity.index === index);
@@ -74,12 +151,15 @@ const App = () => {
     }
 
     if (index === playerIndex) {
-      object = { name: "🔺" };
+      object = { name: "🔺", img: shipIcon };
     }
 
     return (
-      <div style={{ cursor: "pointer" }} onClick={() => movePlayer(index)}>
-        {object.name}
+      <div
+        style={{ cursor: "pointer", height: "16px", width: "16px" }}
+        onClick={() => movePlayer(index)}
+      >
+        {object.img ? <img src={object.img} /> : object.name}
       </div>
     );
   };
@@ -87,4 +167,4 @@ const App = () => {
   return <Grid tiles={tiles} colCount={colCount} renderTile={renderTile} />;
 };
 
-render(<App />, document.body);
+render(<App />, document.getElementById("app"));
