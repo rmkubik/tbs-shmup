4adf987e32210d7c620a37b917b015c339c15f60
Ryan Kubik <rmkubik@me.com>
2021-10-01 23:21:37 -0700

    Main menu and story intro

--- src/index.js
+++ src/index.js
@@ -667,6 +667,8 @@ const App = () => {
   const [hasUsedShipPower, setHasUsedShipPower] = useState(false);
   const [showOptions, setShowOptions] = useState(false);
   const [showCredits, setShowCredits] = useState(false);
+  const [showStory, setShowStory] = useState(false);
+  const [showMainMenu, setShowMainMenu] = useState(true);
   const [enableVfx, setEnableVfx] = useState(true);
   const [isSaveLoaded, setIsSaveLoaded] = useState(false);
 
@@ -1246,6 +1248,54 @@ const App = () => {
               <p className="gameover">LOADING</p>
             </div>
             <p>Loading save data from local storage...</p>
+          </Modal>
+        ) : showStory ? (
+          <Modal>
+            <div className="header">
+              <p className="gameover">THE STORY SO FAR</p>
+            </div>
+            <p>
+              Your planet is dying. You're sprinting frantically to the long
+              abandoned HEW rocket hangar, praying there's still one more ship.
+            </p>
+            <p>
+              You burst through the doors and fluorescent lighting crackles into
+              existence. Across the hangar, you see a derelict Mark ATT vessel.
+            </p>
+            <p>
+              You sprint onto the ship. He's seen better days, but he'll have to
+              do. Your only chance now is to get out of this system, one sector
+              at a time.
+            </p>
+            <p>You gun the engine and fly off into the stars.</p>
+            <p>Good luck, Captain.</p>
+            <div className="button-container">
+              <button
+                onClick={() => {
+                  setShowStory(false);
+                }}
+              >
+                Play
+              </button>
+            </div>
+          </Modal>
+        ) : showMainMenu ? (
+          <Modal>
+            <div className="header">
+              <p className="gameover">ROCKET JOCKEY</p>
+            </div>
+            <p>Captain Pick-a-Card's Great Escape</p>
+
+            <div className="button-container">
+              <button
+                onClick={() => {
+                  setShowStory(true);
+                  setShowMainMenu(false);
+                }}
+              >
+                Start
+              </button>
+            </div>
           </Modal>
         ) : showCredits ? (
           <Modal>
