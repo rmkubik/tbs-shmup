ef36378a7657f2481594b8cc6749f6764deb13dc
Ryan Kubik <rmkubik@me.com>
2021-10-01 23:32:28 -0700

    Add win screen

--- src/index.js
+++ src/index.js
@@ -1412,6 +1412,28 @@ const App = () => {
               </button>
             </div>
           </Modal>
+        ) : gameState === "victory" && winStreak === sectors.length - 1 ? (
+          <Modal>
+            <div className="header">
+              <p className="gameover">
+                <span className="positive">YOU ESCAPED</span>
+              </p>
+              <p className="streak">
+                All {winStreak + 1} sectors are behind you now.
+              </p>
+            </div>
+            <p>Congratulations, Captain!</p>
+            <div className="button-container">
+              <button
+                onClick={() => {
+                  startNewRound();
+                  setShowMainMenu(true);
+                }}
+              >
+                Menu
+              </button>
+            </div>
+          </Modal>
         ) : gameState === "victory" ? (
           <Modal>
             <div className="header">
