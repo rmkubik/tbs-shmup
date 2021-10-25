4856e56063f2317021d0f55f1de1eda627cdaf19
Ryan Kubik <rmkubik@me.com>
2021-10-24 23:11:48 -0700

    Add sectors repeat example

--- src/data/sectors.js
+++ src/data/sectors.js
@@ -1,4 +1,14 @@
-const sectors = [
+const concat = (arrays) => {
+  return [].concat.apply([], arrays);
+};
+
+const repeat = (array, count) => {
+  const arrays = new Array(count).fill("").map(() => array);
+
+  return concat(arrays);
+};
+
+let sectors = [
   // Zone 1
   { conditions: ["lightAsteroids"] },
   // Zone 2
@@ -35,5 +45,25 @@ const sectors = [
     conditions: ["heavyAsteroids", "malfunctioning", "stalling", "nebula"],
   },
 ];
+
+// sectors = [
+//   { conditions: ["mediumAsteroids", "checkpoint"] },
+//   { conditions: ["mediumAsteroids"] },
+//   { conditions: ["mediumAsteroids"] },
+//   { conditions: ["mediumAsteroids"] },
+//   { conditions: ["heavyAsteroids"] },
+//   ...repeat(
+//     [
+//       { conditions: ["mediumAsteroids"] },
+//       { conditions: ["mediumAsteroids"] },
+//       { conditions: ["mediumAsteroids"] },
+//       { conditions: ["mediumAsteroids"] },
+//       { conditions: ["heavyAsteroids"] },
+//     ],
+//     3
+//   ),
+// ];
+
+// console.log(sectors);
 
 export default sectors;
