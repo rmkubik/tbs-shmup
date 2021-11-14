b0a8c54cf40c648ea89884e2ddd470567fab82ff
Ryan Kubik <rmkubik@me.com>
2021-11-14 09:36:59 -0800

    Apply new util asteroid fns to light/heavy

--- src/data/conditions/heavyAsteroids.js
+++ src/data/conditions/heavyAsteroids.js
@@ -1,40 +1,24 @@
-import randInt from "../../utils/randInt";
-import WeightedMap from "../../utils/WeightedMap";
+import {
+  pickNextSpawnsWithWeightedMap,
+  spawnEntitiesWithRandomSpeed,
+} from "./utils";
 
-const heavyAsteroids = {
+const mediumAsteroids = {
   chooseNextSpawns: (colCount, sector, turnCount, spawnPattern) => {
-    const spawnCountMap = new WeightedMap({
-      6: 10,
-      7: 20,
-      8: 40,
-      9: 20,
-      10: 10,
-    });
-
-    const spawnCount = parseInt(spawnCountMap.pickRandom());
-
-    const indices = [];
-
-    let iterationCount = 0;
-
-    // Try up to 100 times to place spawnCount amount of asteroids
-    while (indices.length < spawnCount && iterationCount < 100) {
-      iterationCount += 1;
-
-      // Choose random int in top row
-      const spawnIndex = randInt(0, colCount - 1);
-
-      if (indices.some((index) => index === spawnIndex)) {
-        // Skip indices we've already chosen
-        continue;
-      }
-
-      indices.push(spawnIndex);
-    }
-
-    return indices;
+    return pickNextSpawnsWithWeightedMap(
+      {
+        6: 10,
+        7: 20,
+        8: 40,
+        9: 20,
+        10: 10,
+      },
+      colCount
+    );
   },
-  spawnEntities: (nextSpawns) => {},
+  spawnEntities: (nextSpawns) => {
+    return spawnEntitiesWithRandomSpeed(nextSpawns);
+  },
 };
 
-export default heavyAsteroids;
+export default mediumAsteroids;
--- src/data/conditions/lightAsteroids.js
+++ src/data/conditions/lightAsteroids.js
@@ -1,39 +1,23 @@
-import randInt from "../../utils/randInt";
-import WeightedMap from "../../utils/WeightedMap";
+import {
+  pickNextSpawnsWithWeightedMap,
+  spawnEntitiesWithRandomSpeed,
+} from "./utils";
 
-const lightAsteroids = {
+const mediumAsteroids = {
   chooseNextSpawns: (colCount, sector, turnCount, spawnPattern) => {
-    const spawnCountMap = new WeightedMap({
-      3: 40,
-      4: 30,
-      5: 25,
-      6: 5,
-    });
-
-    const spawnCount = parseInt(spawnCountMap.pickRandom());
-
-    const indices = [];
-
-    let iterationCount = 0;
-
-    // Try up to 100 times to place spawnCount amount of asteroids
-    while (indices.length < spawnCount && iterationCount < 100) {
-      iterationCount += 1;
-
-      // Choose random int in top row
-      const spawnIndex = randInt(0, colCount - 1);
-
-      if (indices.some((index) => index === spawnIndex)) {
-        // Skip indices we've already chosen
-        continue;
-      }
-
-      indices.push(spawnIndex);
-    }
-
-    return indices;
+    return pickNextSpawnsWithWeightedMap(
+      {
+        3: 40,
+        4: 30,
+        5: 25,
+        6: 5,
+      },
+      colCount
+    );
   },
-  spawnEntities: (nextSpawns) => {},
+  spawnEntities: (nextSpawns) => {
+    return spawnEntitiesWithRandomSpeed(nextSpawns);
+  },
 };
 
-export default lightAsteroids;
+export default mediumAsteroids;
